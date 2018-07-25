5938bb01f3e100a9129a0e79e21cb0d4fc2e094e
Matt Hall <matthall8@gmail.com>
2018-07-25 11:55:35 +0300

    Added functionality to connect frontend to backed

--- public/index.js
+++ public/index.js
@@ -1,25 +1,28 @@
 // Event listener to trigger function when the user has typed in the main input form of the page
-
 var starForm = document.getElementById("star-finder");
 starForm.addEventListener(
   "keyup",
-  fetchDataFromServer(starForm.input, appendData)
+  fetchDataFromServer(appendDataFromServer)
+  // fetchDataFromServer("Why", appendDataFromServer)
 );
 
 // Function to send a request for data from the server
 
-function fetchDataFromServer(searchValue, callback) {
+function fetchDataFromServer() {
+  console.log(starForm.value);
   var xhr = new XMLHttpRequest();
+  var url = "/search" + "?q=" + starForm.value;
   xhr.onreadystatechange = function() {
     if (xhr.status === 200 && xhr.readyState == 4) {
       var response = JSON.parse(xhr.responseText);
-      return callback(response);
     }
   };
-  xhr.open("GET", searchValue);
+  xhr.open("GET", url);
   xhr.send();
 }
 
 // Function to add all of the user's suggestions to the DOM.
 
-function appendDataFromServer(response) {}
+function appendDataFromServer(response) {
+  return response;
+}
