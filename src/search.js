2756e80365c39c7f499a0d08f9598c9a6a6d34c4
Matt Hall <matthall8@gmail.com>
2018-07-25 18:53:52 +0300

    Search now only displays 5 results

--- src/search.js
+++ src/search.js
@@ -2,17 +2,17 @@ var url = require("url");
 var data = require("../data.json").stars;
 
 function wordSearch(str) {
-    var output = [];
-    for (var i = 0, l = data.length; i < l && output.length < 85; i++) {
+  var output = [];
+  for (var i = 0, l = data.length; i < l && output.length < 5; i++) {
     var word = data[i];
-      if (str === ""){
-        return output = [];
-      }
-      if (word.indexOf(str) === 0) {
-        output.push(word);
-      }
+    if (str === "") {
+      return (output = []);
     }
-    return output;
+    if (word.indexOf(str) === 0) {
+      output.push(word);
+    }
+  }
+  return output;
 }
 
 module.exports = wordSearch;
